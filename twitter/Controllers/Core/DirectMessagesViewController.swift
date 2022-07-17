4f75fe14f20a4ce81e264f7d14478e2acddf2234
Лебединский Денис <dlebedinsky@gid.ru>
2022-07-18 01:21:14 +0300

    Initial Commit

---
+++ twitter/Controllers/Core/DirectMessagesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DirectMessagesViewController.swift
+//  twitter
+//
+//  Created by Denis Lebedinsky on 05.07.2022.
+//
+
+import UIKit
+
+class DirectMessagesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
